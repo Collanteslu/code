d2866a23a9e01a317e4ae92ac1fd3484119c6d07
Luis <Collanteslu@gmail.com>
2021-02-10 13:13:15 +0100

    first commit

---
+++ application/models/Train_model.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Train_model extends CI_Model
+{
+
+    public function login()
+    {
+
+        return true;
+    }
+}
+                        
+/* End of file train_model.php */
---
+++ application/models/train/Train2_model.php
@@ -0,0 +1,14 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Train2_model extends CI_Model
+{
+
+    public function train2_function()
+    {
+        return true;
+    }
+}
+                        
+/* End of file train2_model.php */
